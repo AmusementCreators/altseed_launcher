48455eb2a85ec48f4c511c60d75441c7e036e2ef
uwe <40294263+UweSilver@users.noreply.github.com>
2023-11-05 08:49:54 +0900

    wip

--- Program.cs
+++ Program.cs
@@ -22,37 +22,26 @@ Engine.Initialize("GameLauncher", 720 / 9 * 16, 720);
 for(int i = 0; i < games.Count; i++)
     coverTex.Add(Texture2D.LoadStrict("./" + games[i] + "/cover.png"));
 
-SpriteNode coverNode = new SpriteNode();
-coverNode.Position = new Vector2F(0, 0);
-Engine.AddNode(coverNode);
-void setCover(int idx)
+List<SpriteNode> coverNodes = new();
+Vector2F idx2pos(int i)
 {
-    coverNode.Texture = coverTex[idx];
-    var scale = new Vector2F(720 / 9 * 16, 720) / coverTex[idx].Size;
+    return new Vector2F(i % 3 * 720 / 9 * 16 / 3, i / 3 * 720 / 3);
+}
+for(int i = 0; i < coverTex.Count; i++)
+{
+    var coverNode = new SpriteNode();
+    coverNode.Position = new Vector2F(i % 3 * 720 / 9 * 16 / 3, i / 3 * 720 / 3);
+    coverNode.Texture = coverTex[i];
+    var scale = new Vector2F(720 / 9 * 16, 720) / 3 / coverTex[i].Size;
     coverNode.Scale = new Vector2F(1, 1) * (scale.X < scale.Y ? scale.X : scale.Y);
+    Engine.AddNode(coverNode);
+    coverNodes.Add(coverNode);
 }
-
-var cancelTokenGen = new CancellationTokenSource();
-async Task moveCover(CancellationToken cancelToken)
-{
-    for(int i = 0; i < 720 / 9 * 16; i++)
-    {
-        coverNode.Position += new Vector2F(1, 0);
-        if (cancelToken.IsCancellationRequested)
-            break;
-        await Task.Delay(10);
-    }
-    coverNode.Position = new Vector2F(0, 0);
-    return;
-}
-
-Task animation = null;
 
 Process? p = null;
 while (Engine.DoEvents())
 {
     Engine.Update();
-    setCover(selectedGameIdx);
 
     if (Engine.Keyboard.GetKeyState(Key.Enter) == ButtonState.Push)
     {
@@ -63,12 +52,18 @@ while (Engine.DoEvents())
     if (Engine.Keyboard.GetKeyState(Key.Left) == ButtonState.Push || Engine.Keyboard.GetKeyState(Key.A) == ButtonState.Push)
     {
         selectedGameIdx = (selectedGameIdx - 1 + games.Count) % games.Count;
-        coverNode.Texture = coverTex[selectedGameIdx];
     }
     if (Engine.Keyboard.GetKeyState(Key.Right) == ButtonState.Push || Engine.Keyboard.GetKeyState(Key.D) == ButtonState.Push)
     {
         selectedGameIdx = (selectedGameIdx + 1 + games.Count) % games.Count;
-        coverNode.Texture = coverTex[selectedGameIdx];
+    }
+    if(Engine.Mouse.GetMouseButtonState(MouseButton.ButtonLeft) == ButtonState.Release)
+    {
+        var pos = Engine.Mouse.Position;
+        selectedGameIdx = ((int)pos.X / (720 / 9 * 16 / 3)) + (int)pos.Y / (720 / 3) * 3;
+        selectedGameIdx = 0 > selectedGameIdx ? 0 :(games.Count - 1 < selectedGameIdx ? games.Count-1 : selectedGameIdx);
+        var info = new ProcessStartInfo("./" + games[selectedGameIdx] + "/" + games[selectedGameIdx]);
+        p = Process.Start(info);
     }
 
     if (Engine.Keyboard.GetKeyState(Key.Escape) == ButtonState.Push && p != null)
